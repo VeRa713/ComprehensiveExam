0adbdd9b6be0d615d81c66fbc480edcd061ec01e
Vera David <vera.david1214@gmail.com>
2023-01-27 14:44:38 +0800

    implemented interface

--- services/EmployeeService.cs
+++ services/EmployeeService.cs
@@ -4,8 +4,31 @@ using ComprehensiveExam.Conf;
 
 namespace ComprehensiveExam.services
 {
-    public interface EmployeeService : IEmployeeService
+    public class EmployeeService : IEmployeeService
     {
-        
+        public void AddSale(SalesEmployee e, Sale s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Employee e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetAllSalesEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Save(Employee e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
