2cffe7eee97b64ee06e075b524fcb38c2ffcbfdc
Vera David <vera.david1214@gmail.com>
2023-01-27 14:37:18 +0800

    #3 - Created IEmployeeService interface

---
+++ interfaces/IEmployeeService.cs
@@ -0,0 +1,32 @@
+using ComprehensiveExam.Models;
+
+namespace ComprehensiveExam.interfaces
+{
+    public class IEmployeeService
+    {
+        public List<Employee> GetAll()
+        {
+            return null;
+        }
+
+        public List<Employee> GetAllSalesEmployees()
+        {
+            return null;
+        }
+
+        public Employee Save(Employee e)
+        {
+            return null;
+        }
+        public void Delete(Employee e)
+        {
+
+        }
+
+        public void AddSale(SalesEmployee e, Sale s)
+        {
+
+        }
+
+    }
+}
